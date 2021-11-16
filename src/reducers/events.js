5769026422957f585ec9b2234e29e72c1f6085f7
FiroProchainezo003 <takigawayouhei3333@yahoo.co.jp>
2021-11-16 15:45:49 +0900

    途中まで

---
+++ src/reducers/events.js
@@ -0,0 +1,24 @@
+import _ from 'lodash'
+import {
+    READ_EVENTS,
+    READ_EVENT,
+    DELETE_EVENT
+} from "../actions";
+// import * as events from "events";
+
+// const initialState = { value: 0};
+
+export default (events = {}, action) => {
+    switch (action.type) {
+        case READ_EVENT:
+            const data = action.response.data
+            return { ...events, [data.id]: data }
+        case READ_EVENTS:
+            return _.mapKeys(action.response.data, 'id')
+        case DELETE_EVENT:
+            delete events[action.id]
+            return { ...events }
+        default:
+            return events
+    }
+}
